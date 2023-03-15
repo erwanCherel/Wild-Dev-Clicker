8896161587e65fc9d88b3505bde6040fcc1d2778
Bertrand Robert <brobert.bertrand@wanadoo.fr>
2023-03-15 14:13:09 +0100

    add items in JS

---
+++ main.js
@@ -0,0 +1,44 @@
+const items = [
+  {
+    src: "./assets/html.png",
+    name: "HTML",
+    price: 15,
+    count: 0,
+    description: "Gère le contenu de ta page WEB",
+  },
+  {
+    src: "./assets/css.png",
+    name: "CSS",
+    price: 100,
+    count: 0,
+    description: "Ajoute du style",
+  },
+  {
+    src: "./assets/js.png",
+    name: "JS",
+    price: 500,
+    count: 0,
+    description: "Ajoute de l'interaction entre tes éléments",
+  },
+  {
+    src: "./assets/react.png",
+    name: "REACT",
+    price: 2000,
+    count: 0,
+    description: "Utilise les meilleures librairies",
+  },
+  {
+    src: "./assets/sql.png",
+    name: "SQL",
+    price: 7000,
+    count: 0,
+    description: "Apprends à gérer des bases de données",
+  },
+  {
+    src: "./assets/dev.png",
+    name: "DEV",
+    price: 1000000,
+    count: 0,
+    description: "Tu es maintenant un vrai DEV !",
+  },
+];
